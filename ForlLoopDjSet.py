01f641351dce7b3aabdaf3526b24c5dcd5ca346f
ejones001 <ejonesnavy@gmail.com>
2024-05-10 00:54:49 -0500

    For Loop Dj Set

---
+++ ForlLoopDjSet.py
@@ -0,0 +1,6 @@
+# Our playlist of genres
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+
+# Loop through each genre
+for i, genre in enumerate(genres, start=1):
+    print(f"Track {i}: This song belongs to the {genre} genre.")
