6398bb8c92f44cdd991ef21e866b5d046ab7c981
ejones001 <ejonesnavy@gmail.com>
2024-05-10 02:40:36 -0500

    Comprehensions

---
+++ Comprehensions.py
@@ -0,0 +1,8 @@
+# Our playlist of genres
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+
+# List comprehension to create a new list with Music appended to each genre
+genre_with_music = [genre + ' Music' for genre in genres]
+
+# Print the new list
+print(genre_with_music)
