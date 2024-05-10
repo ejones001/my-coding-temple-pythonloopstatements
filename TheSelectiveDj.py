519b02e5fda2f7cf2db056e9474d963a3b5279e5
ejones001 <ejonesnavy@gmail.com>
2024-05-10 02:37:13 -0500

    The Selective Dj

---
+++ TheSelectiveDj.py
@@ -0,0 +1,9 @@
+# playlist of genres
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+
+# Create a sublist of genres from the second to the fourth track
+sublist_genres = genres[1:4]  # Slicing from index 1 to 4 
+
+# Loop through the sublist of genres and print them
+for genre in sublist_genres:
+    print(genre)
